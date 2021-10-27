ccaa21362c5052e5a236473f3a2060dd070b2581
Yakumo Saki <yakumo@ziomatrix.org>
2021-10-27 11:44:19 +0900

    test

--- testrun.sh
+++ testrun.sh
@@ -4,12 +4,12 @@ export AWS_REGION=ap-northeast-3
 
 # WebHooks - Slack
 export SLACK_CHANNEL="#general"
-export SLACK_URL=https://hooks.slack.com/services/T000AAAAA/B0000BBBBB/fafafafafafafafafafa
+export SLACK_URL="Slack incoming webhook url like https://hooks.slack.com/services/~~~~"
 export SLACK_DISP_NAME=ofuroNotify
 export SLACK_ICON_EMOJI=ghost
 
 # WebHooks - Mastodon
-export MASTODON_KEY=ddddffefefwfasdfasfadsfasdfashjgfadsh
+export MASTODON_KEY=dd964bf1a315eaab3d3d86c98f7186c1fff365894624830e20ad2e5b2530c928
 export MASTODON_URL=https://example.com/api/v1/statuses
 
 # WebHooks - Closed API
